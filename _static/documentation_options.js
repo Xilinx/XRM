682a9fce2dee42148874a7fc1af3350793208009
Bin Tu <bin.tu@xilinx.com>
2021-07-14 18:49:05 -0700

    update document for 2021.1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '202002',
+    VERSION: '202101',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
