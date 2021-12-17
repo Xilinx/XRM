23a9214f6720f6a2efe91ac745c1b3c37c2866be
Bin Tu <bin.tu@xilinx.com>
2021-12-16 23:59:57 -0800

    move old doc to 2021.1, add doc of 2021.2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '202101',
+    VERSION: '202102',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
